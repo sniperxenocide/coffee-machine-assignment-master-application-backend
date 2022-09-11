5f4a0feedac42fa509b33286fc1eb955a3168b8c
DELL <fahimbuet1995@gmail.com>
2022-09-11 18:02:37 +0600

    - Role Privilege assignment changed.

---
+++ src/main/resources/db/migration/V13__role_privilege_reassign.sql
@@ -0,0 +1,3 @@
+insert into role(id,name) values
+(6,'Machine Creator'),(7,'Shop Creator'),(8,'Contract Creator'),
+(9,'Contract Deleter');
---
+++ src/main/resources/db/migration/V14__role_privilege_reassign_and_edit.sql
@@ -0,0 +1,5 @@
+update role_privilege set role_id=9,privilege_id=28 where id=28;
+update role_privilege set role_id=8,privilege_id=14 where id=22;
+
+update role_privilege set role_id=7,privilege_id=10 where id=20;
+update role_privilege set role_id=6,privilege_id=5 where id=18;
